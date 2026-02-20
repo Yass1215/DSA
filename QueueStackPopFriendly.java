125ada1f5f5bd36146d8ef7524e4a9f66248be0c
Yass1215 <yasasminimnadi59@gmail.com>
2026-02-20 12:44:22 +0530

    Update QueueStackPopFriendly.java

--- QueueStackPopFriendly.java
+++ QueueStackPopFriendly.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package DSA;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -40,3 +37,4 @@ public class QueueStackPopFriendly {
     }
     
 }
+
