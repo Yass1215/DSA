b855f043b638ce537dba907bbe85f307ff0f9e6c
Yass1215 <yasasminimnadi59@gmail.com>
2026-02-20 12:39:01 +0530

    Update SequencedCollectionAc.java

--- SequencedCollectionAc.java
+++ SequencedCollectionAc.java
@@ -14,6 +14,7 @@ public class SequencedCollectionAc {
         sc.addFirst(5);
         sc.addFirst(2);
         sc.addLast(3);
+        sc.addLast(4);
         System.out.println(sc.getFirst());
         System.out.println(sc.removeLast());
         System.out.println((sc));
@@ -25,3 +26,4 @@ public class SequencedCollectionAc {
     }
     
 }
+
