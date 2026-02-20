b3583a326ec06de5cdb11bd92e8d9233af6ab066
Yass1215 <yasasminimnadi59@gmail.com>
2026-02-20 13:09:37 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,32 +1,11 @@
-
-package DSA;
-
 public class BubbleSort {
-
-    static void bubbleSort(int[] arr) {
-        int n = arr.length;
-
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-
-                if (arr[j] > arr[j + 1]) {
-                    // Swap
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+    public static void bubbleSort(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 0; j < a.length - 1 - i; j++) {
+                if (a[j] > a[j + 1]) {
+                    int tmp = a[j]; a[j] = a[j + 1]; a[j + 1] = tmp;
                 }
             }
         }
     }
-
-    public static void main(String[] args) {
-        int[] arr = {5, 3, 8, 1};
-
-        bubbleSort(arr);
-
-        for (int x : arr)
-            System.out.print(x + " ");
-    }
 }
-    
-
