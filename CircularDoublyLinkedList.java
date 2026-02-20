386832d22ddef10fdfa499cb67b466f4cea887e2
Yass1215 <yasasminimnadi59@gmail.com>
2026-02-20 13:06:36 +0530

    Update CircularDoublyLinkedList.java

--- CircularDoublyLinkedList.java
+++ CircularDoublyLinkedList.java
@@ -1,69 +1,78 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package DSA;
+import java.util.NoSuchElementException;
 
-/**
- *
- * @author DELL
- */
-public class CircularDoublyLinkedList {
-
-    class Node {
-        int data;
-        Node prev, next;
-
-        Node(int data) {
-            this.data = data;
-        }
+public class CircularDoublyLinkedList<T> {
+    private static class Node<T> {
+        T data; Node<T> prev, next;
+        Node(T data) { this.data = data; }
     }
 
-    Node head = null;
+    private Node<T> head; // if not null, head.prev is tail and tail.next is head
+    private int size = 0;
 
-    // Insert at end
-    void insert(int data) {
-        Node newNode = new Node(data);
-
+    public void addFirst(T x) {
+        Node<T> n = new Node<>(x);
         if (head == null) {
-            newNode.next = newNode;
-            newNode.prev = newNode;
-            head = newNode;
-            return;
+            n.next = n.prev = n;
+            head = n;
+        } else {
+            Node<T> tail = head.prev;
+            n.next = head;
+            n.prev = tail;
+            tail.next = n;
+            head.prev = n;
+            head = n;
         }
-
-        Node last = head.prev;
-
-        newNode.next = head;
-        newNode.prev = last;
-
-        last.next = newNode;
-        head.prev = newNode;
+        size++;
     }
 
-    // Display list
-    void display() {
-        if (head == null) return;
-
-        Node temp = head;
-        do {
-            System.out.print(temp.data + " <-> ");
-            temp = temp.next;
-        } while (temp != head);
-
-        System.out.println("(Back to Head)");
+    public void addLast(T x) {
+        if (head == null) { addFirst(x); return; }
+        Node<T> tail = head.prev;
+        Node<T> n = new Node<>(x);
+        n.next = head;
+        n.prev = tail;
+        tail.next = n;
+        head.prev = n;
+        size++;
     }
 
-    public static void main(String[] args) {
-        CircularDoublyLinkedList list = new CircularDoublyLinkedList();
-
-        list.insert(10);
-        list.insert(20);
-        list.insert(30);
-
-        list.display();
+    public T removeFirst() {
+        if (isEmpty()) throw new NoSuchElementException("List is empty");
+        T val = head.data;
+        if (size == 1) {
+            head = null;
+        } else {
+            Node<T> tail = head.prev;
+            head = head.next;
+            head.prev = tail;
+            tail.next = head;
+        }
+        size--;
+        return val;
     }
+
+    public T removeLast() {
+        if (isEmpty()) throw new NoSuchElementException("List is empty");
+        if (size == 1) return removeFirst();
+        Node<T> tail = head.prev;
+        T val = tail.data;
+        Node<T> newTail = tail.prev;
+        newTail.next = head;
+        head.prev = newTail;
+        size--;
+        return val;
+    }
+
+    public T getFirst() {
+        if (isEmpty()) throw new NoSuchElementException("List is empty");
+        return head.data;
+    }
+
+    public T getLast() {
+        if (isEmpty()) throw new NoSuchElementException("List is empty");
+        return head.prev.data;
+    }
+
+    public int size() { return size; }
+    public boolean isEmpty() { return size == 0; }
 }
-
-    
-
