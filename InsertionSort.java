37a98fce70fef924a659b34848adcdea59228a50
Yass1215 <yasasminimnadi59@gmail.com>
2026-02-20 13:10:21 +0530

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,36 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package DSA;
-
-
 public class InsertionSort {
-
-    static void insertionSort(int[] arr) {
-        int n = arr.length;
-
-        for (int i = 1; i < n; i++) {
-            int key = arr[i];
+    public static void insertionSort(int[] a) {
+        for (int i = 1; i < a.length; i++) {
+            int key = a[i];
             int j = i - 1;
-
-            // Shift elements to the right
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
+            while (j >= 0 && a[j] > key) {
+                a[j + 1] = a[j];
                 j--;
             }
-
-            arr[j + 1] = key;
+            a[j + 1] = key;
         }
     }
-
-    public static void main(String[] args) {
-        int[] arr = {7, 4, 5, 2};
-
-        insertionSort(arr);
-
-        for (int x : arr)
-            System.out.print(x + " ");
-    }
-    
 }
