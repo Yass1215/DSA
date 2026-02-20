83c26667fd2e70d545e894254d603324f326fb0d
Yass1215 <yasasminimnadi59@gmail.com>
2026-02-20 13:10:44 +0530

    Update OptimizedInsertionSort.java

--- OptimizedInsertionSort.java
+++ OptimizedInsertionSort.java
@@ -1,35 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package DSA;
-
-class OptimizedInsertionSort {
-
-    static void insertionSort(int[] arr) {
-        int n = arr.length;
-
-        for (int i = 1; i < n; i++) {
-            int key = arr[i];
-            int j = i - 1;
-
-            // Shift only when required
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-
-            arr[j + 1] = key;
+public class OptimizedInsertionSort {
+    public static void binaryInsertionSort(int[] a) {
+        for (int i = 1; i < a.length; i++) {
+            int key = a[i];
+            int pos = lowerBound(a, 0, i, key);
+            // shift right
+            for (int j = i; j > pos; j--) a[j] = a[j - 1];
+            a[pos] = key;
         }
     }
 
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 5};
-
-        insertionSort(arr);
-
-        for (int x : arr)
-            System.out.print(x + " ");
+    // first index in [lo, hi) where a[idx] >= key
+    private static int lowerBound(int[] a, int lo, int hi, int key) {
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (a[mid] < key) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo;
     }
 }
-
